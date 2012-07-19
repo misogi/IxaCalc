f7b7995271ad256ff814c67986575b520eb921b3
ryudo_n <nkgw.ry@gmail.com>
2012-07-19 13:49:34 +0900

    コメント追加

--- IxaCalc/Design/DesignDataService.cs
+++ IxaCalc/Design/DesignDataService.cs
@@ -3,6 +3,9 @@ using IxaCalc.Model;
 
 namespace IxaCalc.Design
 {
+    /// <summary>
+    /// データ取得メソッド
+    /// </summary>
     public class DesignDataService : IDataService
     {
         public void GetData(Action<DataItem, Exception> callback)
