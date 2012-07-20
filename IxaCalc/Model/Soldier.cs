d00f5ddee5da2c825c2709b2bcea3cccbbc55113
ryudo_n <nkgw.ry@gmail.com>
2012-07-20 13:06:14 +0900

    兵士タイプクラス作成

---
+++ IxaCalc/Model/Soldier.cs
@@ -0,0 +1,18 @@
+﻿namespace IxaCalc.Model
+{
+    public class Soldier
+    {
+        public int Attack { get; set; }
+
+        public int Defence { get; set; }
+
+        public string Name { get; set; }
+
+        public Soldier(string name, int atk, int def)
+        {
+            Attack = atk;
+            Defence = def;
+            Name = name;
+        }
+    }
+}
