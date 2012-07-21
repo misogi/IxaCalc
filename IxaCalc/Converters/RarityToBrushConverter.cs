bc94c643c46f4ef93185454179b59dc041860740
ryudo_n <nkgw.ry@gmail.com>
2012-07-22 00:35:23 +0900

    レアリティで背景色を変えた 全体背景つけた

---
+++ IxaCalc/Converters/RarityToBrushConverter.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Data;
+
+namespace IxaCalc.Converters
+{
+    using System;
+    using System.Windows.Media;
+    using System.Globalization;
+    using IxaCalc.Enums;
+
+    public class RarityToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return new SolidColorBrush(Color.FromArgb(255, 220, 220, 220));
+            }
+            var rarity = (RarityRank)value;
+            Color col = Colors.Black;
+            switch(rarity)
+            {
+                case RarityRank.Common:
+                    col = Color.FromArgb(255, 130, 130, 240);
+                    break;
+                case RarityRank.Uncommon:
+                    col = Color.FromArgb(255, 230, 230, 110);
+                    break;
+                case RarityRank.Rare:
+                    col = Color.FromArgb(255, 230, 110, 110);
+                    break;
+                case RarityRank.SuperRare:
+                    col = Color.FromArgb(255, 90, 90, 90);
+                    break;
+                case RarityRank.UltraRare:
+                    col = Color.FromArgb(255, 180, 180, 180);
+                    break;
+            }
+
+            var brush = new SolidColorBrush(col);
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            throw new NotImplementedException();
+
+        }
+    }
+}
